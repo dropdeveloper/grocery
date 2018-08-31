8d4730ce6619a65cea1ffdfd70590617762056f9
zaroor.tech@gmail.com <ijas1234m>
2018-08-31 13:35:24 +0400

    initial commit

--- app/src/main/java/com/imarahtech/grocery/activity/IntroActivity.java
+++ app/src/main/java/com/imarahtech/grocery/activity/IntroActivity.java
@@ -28,7 +28,7 @@ public class IntroActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
 
-
+//
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             Window w = getWindow(); // in Activity's onCreate() for instance
             w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
